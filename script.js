17d2a7e3a72aa91c7aaf296394bdb61664c78e03
Bex-G <bexgiacinto@gmail.com>
2023-07-26 17:56:10 -0700

    Add button to remove selected row.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let myLibrary = [
     {title: "Gideon the Ninth", author: 'Tamsyn Muir', pages: 325, read: "yes"},
     {title: "Harrow the Ninth", author: 'Tamsyn Muir', pages: 482, read: "yes"},
     {title: "Nona the Ninth", author: 'Tamsyn Muir', pages: 512, read: "yes"},
-    {title: "Alecto the Ninth", author: 'Tamsyn Muir', pages: "", read: "no"},
+    {title: "Alecto the Ninth", author: 'Tamsyn Muir', pages: 681, read: "no"},
 ];
 
 function Book(title, author, pages, read) {
@@ -18,39 +18,54 @@ function addToLibrary() {
     addToTable();
 };
 
-//display any existing myLibrary entries
+//display any existing myLibrary entries w/ buttons to change read status or delete rows
 
 const table = document.querySelector('#bookTable');
 
 for (let i = 0; i < myLibrary.length; i++) {
     newRow = table.insertRow();
+    newRow.setAttribute('id', [i]);
 
-let newCell1 = newRow.insertCell();
-let newCell2 = newRow.insertCell();
-let newCell3 = newRow.insertCell();
-let newCell4 = newRow.insertCell();
+    let newCell1 = newRow.insertCell();
+    let newCell2 = newRow.insertCell();
+    let newCell3 = newRow.insertCell();
+    let newCell4 = newRow.insertCell();
 
-let newText1 = document.createTextNode(myLibrary[i].title);
-newCell1.appendChild(newText1);
+    let newText1 = document.createTextNode(myLibrary[i].title);
+    newCell1.appendChild(newText1);
 
-let newText2 = document.createTextNode(myLibrary[i].author);
-    newCell2.appendChild(newText2);
+    let newText2 = document.createTextNode(myLibrary[i].author);
+        newCell2.appendChild(newText2);
 
-let newText3 = document.createTextNode(myLibrary[i].pages);
-    newCell3.appendChild(newText3);
+    let newText3 = document.createTextNode(myLibrary[i].pages);
+        newCell3.appendChild(newText3);
 
-    const readButton = document.createElement('button');
-    readButton.setAttribute('id','para-1');
-    readButton.innerText = myLibrary[i].read;
+        const readButton = document.createElement('button');
+        readButton.setAttribute('id','readButton');
+        readButton.innerText = myLibrary[i].read;
+        readButton.setAttribute('class', readButton.innerText);
 
-    readButton.addEventListener('click', (e) => {
-        if (readButton.innerText === "yes") {
-            readButton.innerText = "no";
-        } else if (readButton.innerText === "no") {
-            readButton.innerText = "yes";
-        }
-    })
-    newCell4.appendChild(readButton);
+        readButton.addEventListener('click', (e) => {
+            if (readButton.innerText === "yes") {
+                readButton.innerText = "no";
+                readButton.setAttribute('class', 'no')
+            } else if (readButton.innerText === "no") {
+                readButton.innerText = "yes";
+                readButton.setAttribute('class', 'yes')
+            }
+        })
+        newCell4.appendChild(readButton);
+
+    let newCell5 = newRow.insertCell();
+
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('id','deleteButton');
+    deleteButton.innerText = "remove";
+    deleteButton.addEventListener('click', (e) => {
+        document.getElementById([i]).remove();
+        myLibrary.splice(i, 1)
+    });
+newCell5.appendChild(deleteButton);
 };
 
 // manage form visibility and actions
@@ -64,14 +79,14 @@ function closeForm() {
 };
 
 function clearForm() {
-    document.getElementById("form-container").reset();
+    document.getElementById("formContainer").reset();
 };
 
-document.getElementById('new-book').addEventListener('click', (e) => {
+document.getElementById('newBook').addEventListener('click', (e) => {
     openForm();
 });
 
-document.getElementById('submit-btn').addEventListener('click', (e) => {
+document.getElementById('submitButton').addEventListener('click', (e) => {
     e.preventDefault();
     addToLibrary();
     clearForm();
@@ -87,6 +102,7 @@ function addToTable() {
     const table = document.querySelector('#bookTable');
 
     newRow = table.insertRow();
+    newRow.setAttribute('id', [i]);
 
     let newCell1 = newRow.insertCell();
     let newCell2 = newRow.insertCell();
@@ -103,15 +119,30 @@ function addToTable() {
         newCell3.appendChild(newText3);
 
     const readButton = document.createElement('button');
-    readButton.setAttribute('id','para-1');
+    readButton.setAttribute('id','readButton');
     readButton.innerText = myLibrary[i].read;
-    
+    readButton.setAttribute('class', readButton.innerText);
+
     readButton.addEventListener('click', (e) => {
         if (readButton.innerText === "yes") {
             readButton.innerText = "no";
+            readButton.setAttribute('class', 'no');
         } else if (readButton.innerText === "no") {
             readButton.innerText = "yes";
+            readButton.setAttribute('class', 'yes');
         }
     })
     newCell4.appendChild(readButton);
+
+    let newCell5 = newRow.insertCell();
+
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('id','deleteButton');
+    deleteButton.innerText = "remove";
+    deleteButton.addEventListener('click', (e) => {
+        document.getElementById([i]).remove();
+        myLibrary.splice(i, 1)
+    });
+newCell5.appendChild(deleteButton);
 };
+
