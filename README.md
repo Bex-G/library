64072061586f18e5c5ec4ec2583bf4c41a2d0a9a
Bex-G <bexgiacinto@gmail.com>
2026-02-23 20:42:30 -0800

    Reduced JS redundancy and improved UX.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is a simple application I designed to use object constructors, form input and DOM manipulation methods to create a dynamic table display of object values.
+
+---
+
+Feb. 2026 update:
+
+I reworked the Javascript to eliminate redundancy and improve readability, and I refreshed the CSS and added a simple drawer animation for a nicer UX.
--- script.js
+++ script.js
@@ -1,167 +1,146 @@
 myLibrary = [
-  { title: "Gideon the Ninth", author: "Tamsyn Muir", readStatus: "read" },
+  {
+    title: "Gideon the Ninth",
+    author: "Tamsyn Muir",
+    status: "read",
+    id: crypto.randomUUID(),
+  },
   {
     title: "The Divine Comedy",
     author: "Dante Alighieri",
-    readStatus: "not read",
+    status: "tbr",
+    id: crypto.randomUUID(),
   },
   {
     title: "Good Omens",
     author: "Neil Gaiman, Terry Pratchett",
-    readStatus: "read",
+    status: "read",
+    id: crypto.randomUUID(),
   },
   {
     title: "The Song of Achilles",
     author: "Madeline Miller",
-    readStatus: "read",
+    status: "read",
+    id: crypto.randomUUID(),
   },
 ];
 
 class Book {
-  constructor(title, author, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.readStatus = readStatus;
+  constructor(title, author, status, id) {
+    if (!new.target) {
+      throw Error("You must include the 'new' keyword.");
+    } else {
+      this.title = title;
+      this.author = author;
+      this.status = status;
+      this.id = id;
+    }
   }
 }
 
-function addToLibrary() {
-  let newBook = new Book(
-    bookForm.title.value,
-    bookForm.author.value,
-    bookForm.readStatus.value
+document.addEventListener("DOMContentLoaded", function () {
+  myLibrary.forEach((book) => {
+    addBookToDisplay(book);
+  });
+});
+
+function addBookToDisplay(book) {
+  let tr = document.createElement("tr");
+
+  let tdTitle = document.createElement("td");
+  tdTitle.textContent = book.title;
+
+  let tdAuthor = document.createElement("td");
+  tdAuthor.textContent = book.author;
+
+  let tdStatus = document.createElement("td");
+  let statusBtn = document.createElement("button");
+  statusBtn.textContent = book.status;
+  statusBtn.classList.add(book.status);
+  statusBtn.addEventListener("click", (event) => {
+    handleStatus(event, statusBtn, book);
+  });
+  tdStatus.appendChild(statusBtn);
+
+  let tdDelete = document.createElement("td");
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.classList.add("deleteBtn");
+  deleteBtn.addEventListener("click", (event) => {
+    handleDelete(event, book, tr);
+  });
+  tdDelete.appendChild(deleteBtn);
+
+  tr.appendChild(tdTitle);
+  tr.appendChild(tdAuthor);
+  tr.appendChild(tdStatus);
+  tr.appendChild(tdDelete);
+  document.getElementById("display").appendChild(tr);
+  document.getElementById("displayContainter");
+  tr.scrollIntoView();
+}
+
+function handleStatus(event, statusBtn, book) {
+  if (
+    event.type === "click" ||
+    (event.type === "keyup" && event.key === "Enter")
+  ) {
+    if (book.status === "tbr") {
+      book.status = "read";
+    } else {
+      book.status = "tbr";
+    }
+    statusBtn.textContent = book.status;
+    statusBtn.classList.toggle("tbr");
+    statusBtn.classList.toggle("read");
+  }
+}
+
+function handleDelete(event, book, tr) {
+  if (
+    event.type === "click" ||
+    (event.type === "keyup" && event.key === "Enter")
+  ) {
+    const indexToRemove = myLibrary.findIndex((book) => this.id === book.id);
+    myLibrary.splice(indexToRemove, 1);
+    document.getElementById("display").removeChild(tr);
+  }
+}
+
+// form event and visibility handling
+
+document.getElementById("drawerBtn").addEventListener("click", () => {
+  document.getElementById("formContainer").classList.toggle("on-screen");
+  document.getElementById("drawerBtn").classList.toggle("active");
+});
+
+document.getElementById("cancelBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+  resetForm();
+  document.getElementById("formContainer").classList.toggle("on-screen");
+  document.getElementById("drawerBtn").classList.toggle("active");
+});
+
+function resetForm() {
+  document.getElementById("titleInput").value = "";
+  document.getElementById("authorInput").value = "";
+  document.getElementById("tbr").checked = true;
+  document.getElementById("titleInput").focus();
+}
+
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  resetForm();
+});
+
+function addBookToLibrary() {
+  let book = new Book(
+    bookForm.titleInput.value,
+    bookForm.authorInput.value,
+    bookForm.statusInput.value,
+    crypto.randomUUID(),
   );
-  myLibrary.push(newBook);
-  addToTable();
-}
-
-//display any existing myLibrary entries w/ buttons to change read status or delete rows
-
-const table = document.querySelector("#bookTable");
-
-for (let i = 0; i < myLibrary.length; i++) {
-  newRow = table.insertRow();
-  newRow.setAttribute("id", [i]);
-  newRow.setAttribute("class", "row");
-
-  let newCell1 = newRow.insertCell();
-  newCell1.setAttribute("class", "book-title");
-  let newText1 = document.createTextNode(myLibrary[i].title);
-  newCell1.appendChild(newText1);
-
-  let newCell2 = newRow.insertCell();
-  newCell2.setAttribute("class", "book-author");
-  let newText2 = document.createTextNode(myLibrary[i].author);
-  newCell2.appendChild(newText2);
-
-  let newCell3 = newRow.insertCell();
-  newCell3.setAttribute("class", "book-status");
-
-  const readButton = document.createElement("button");
-  readButton.setAttribute("id", "statusButton");
-  readButton.innerText = myLibrary[i].readStatus;
-
-  readButton.addEventListener("click", () => {
-    if (readButton.innerText === "read") {
-      readButton.innerText = "not read";
-    } else if (readButton.innerText === "not read") {
-      readButton.innerText = "read";
-    }
-  });
-  newCell3.appendChild(readButton);
-
-  let newCell4 = newRow.insertCell();
-  newCell4.setAttribute("class", "book-delete");
-
-  const deleteButton = document.createElement("button");
-  deleteButton.setAttribute("id", "deleteButton");
-  deleteButton.innerText = "delete";
-  deleteButton.addEventListener("click", () => {
-    document.getElementById([i]).remove();
-    myLibrary.splice(i, 1);
-  });
-  newCell4.appendChild(deleteButton);
-}
-
-// manage form visibility and actions
-
-function openForm() {
-  document.getElementById("formContainer").style.display = "flex";
-  document.getElementById("newButton").style.display = "none";
-}
-
-function closeForm() {
-  document.getElementById("formContainer").style.display = "none";
-  document.getElementById("newButton").style.display = "block";
-}
-
-function clearForm() {
-  document.getElementById("myForm").reset();
-}
-
-document.getElementById("newButton").addEventListener("click", () => {
-  openForm();
-});
-
-document.getElementById("myForm").addEventListener("submit", (e) => {
-  e.preventDefault();
-  addToLibrary();
-  clearForm();
-  closeForm();
-});
-
-document.getElementById("cancelButton").addEventListener("click", (e) => {
-  e.preventDefault();
-  clearForm();
-  closeForm();
-});
-
-//add newBook values to table display
-
-function addToTable() {
-  let i = myLibrary.length - 1;
-
-  const table = document.querySelector("#bookTable");
-
-  newRow = table.insertRow();
-  newRow.setAttribute("id", [i]);
-  newRow.setAttribute("class", "row");
-
-  let newCell1 = newRow.insertCell();
-  newCell1.setAttribute("class", "book-title");
-  let newText1 = document.createTextNode(myLibrary[i].title);
-  newCell1.appendChild(newText1);
-
-  let newCell2 = newRow.insertCell();
-  newCell2.setAttribute("class", "book-author");
-  let newText2 = document.createTextNode(myLibrary[i].author);
-  newCell2.appendChild(newText2);
-
-  let newCell3 = newRow.insertCell();
-  newCell3.setAttribute("class", "book-status");
-
-  const readButton = document.createElement("button");
-  readButton.setAttribute("id", "statusButton");
-  readButton.innerText = myLibrary[i].readStatus;
-
-  readButton.addEventListener("click", () => {
-    if (readButton.innerText === "read") {
-      readButton.innerText = "not read";
-    } else if (readButton.innerText === "not read") {
-      readButton.innerText = "read";
-    }
-  });
-  newCell3.appendChild(readButton);
-
-  let newCell4 = newRow.insertCell();
-  newCell4.setAttribute("class", "book-delete");
-
-  const deleteButton = document.createElement("button");
-  deleteButton.setAttribute("id", "deleteButton");
-  deleteButton.innerText = "delete";
-  deleteButton.addEventListener("click", () => {
-    document.getElementById([i]).remove();
-    myLibrary.splice(i, 1);
-  });
-  newCell4.appendChild(deleteButton);
+  myLibrary.push(book);
+  addBookToDisplay(book);
 }
